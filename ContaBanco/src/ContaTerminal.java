cb3e590bfca0fe9b7c93c773caf13a0562b2df9d
rafaelrodrigues <rafaelneziao@gmail.com>
2025-02-14 23:21:13 -0300

    feat: cria método para entrada de dados da conta bancária via terminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o número da Agência!");
+        int numero = scanner.nextInt(); 
+
+        System.out.println("Por favor, digite o nome da Agência!");
+        String agencia = scanner.next();
+        scanner.nextLine();
+        
+        System.out.println("Por favor, digite o seu nome completo!");
+        String nomeCliente = scanner.nextLine();
+        
+        System.out.println("Por favor, digite o seu saldo!");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já estpa disponível para saque");
     }
 }
