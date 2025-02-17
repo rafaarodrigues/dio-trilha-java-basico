20d18940cb79d725211a6c5db92e1bf9d2f9ed60
rafaelrodrigues <rafaelneziao@gmail.com>
2025-02-17 12:31:20 -0300

    feat: cria classe de exceção ParametroInvalidosException, cria método contador e faz o tratamento da exceção se o primeiro número for maior que o segundo

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class Contador {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args)  {
+        Scanner terminalScanner = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número: ");
+        int numeroUm = terminalScanner.nextInt();
+
+        System.out.println("Digite o segundo número: ");
+        int numeroDois = terminalScanner.nextInt();
+
+        try {
+            contar(numeroUm, numeroDois);
+        } catch (ParametrosInvalidosException exception) {
+            // TODO: handle exception
+        }
+        
+        
+        // se o PRIMEIRO NUMERO > SEGUNDO NUMERO lançar uma EXCEÇÃO CUSTOMIZADA com a mensagem "O segundo parâmetro deve ser maior que o primeiro"
+    }
+
+    public static void contar(int numeroUm, int numeroDois) throws ParametrosInvalidosException{
+        if (numeroUm > numeroDois) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+        } else {
+            int contagem = numeroDois - numeroUm;
+            for(int inicializador = 0; inicializador < contagem; inicializador++) {
+                System.out.println("Imprimindo o número " + numeroUm + ", Imprimindo o número " + numeroDois);
+            }
+        }
     }
 }
